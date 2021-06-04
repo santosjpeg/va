c98b61357dbde526692e92e46bac71160df0093f
aaron <santosaaron2004@gmail.com>
2021-06-03 22:36:09 -0400

    simple framework

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,13 @@
 import torch 
+
+import nltk
+from nltk.tokenize import word_tokenize
+
+def main():
+    while(True):
+        response = input("Enter: ")
+        if response == 'bye':
+            break
+
+if __name__ == '__main__':
+    main()
