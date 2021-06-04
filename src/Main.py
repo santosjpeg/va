e9021a26399d6e4d75822748e72c0328eb6e9938
aaron <santosaaron2004@gmail.com>
2021-06-03 22:38:23 -0400

    start reply function

--- src/Main.py
+++ src/Main.py
@@ -3,11 +3,16 @@ import torch
 import nltk
 from nltk.tokenize import word_tokenize
 
+def reply(res):
+    print('filler')
+
 def main():
     while(True):
         response = input("Enter: ")
         if response == 'bye':
             break
+        else:
+            reply(response)
 
 if __name__ == '__main__':
     main()
